TenX-UDC/vendor_lineage

vendor: Init TenX-OS based on Lineage-OS
Change-Id: I53c407da455885af7029853ff635090f34b01f0c
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

build/tasks/bacon.mk
@@ -16,12 +16,21 @@
 # -----------------------------------------------------------------
 # Lineage OTA update package
 
-LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/lineage-$(LINEAGE_VERSION).zip
+LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
 bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
+	$(hide) mv -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
+	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
+	@echo -e ""
+	@echo -e ${CL_GRN}"████████╗███████╗███╗   ██╗██╗  ██╗    "
+	@echo -e ${CL_GRN}"╚══██╔══╝██╔════╝████╗  ██║╚██╗██╔╝    "
+	@echo -e ${CL_GRN}"   ██║   █████╗  ██╔██╗ ██║ ╚███╔╝     "
+	@echo -e ${CL_GRN}"   ██║   ██╔══╝  ██║╚██╗██║ ██╔██╗     "
+	@echo -e ${CL_GRN}"   ██║   ███████╗██║ ╚████║██╔╝ ██╗    "
+	@echo -e ${CL_GRN}"   ╚═╝   ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝    "
+	@echo -e ""
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2

TenX-UDC/vendor_tenx

build/tasks/bacon.mk
@@ -16,13 +16,22 @@
 # -----------------------------------------------------------------
 # Lineage OTA update package
 
-LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/lineage-$(LINEAGE_VERSION).zip
+LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(TENX_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 $(LINEAGE_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
+	$(hide) mv -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
+	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
+	@echo -e ""
+	@echo -e ${CL_GRN}"████████╗███████╗███╗   ██╗██╗  ██╗    "
+	@echo -e ${CL_GRN}"╚══██╔══╝██╔════╝████╗  ██║╚██╗██╔╝    "
+	@echo -e ${CL_GRN}"   ██║   █████╗  ██╔██╗ ██║ ╚███╔╝     "
+	@echo -e ${CL_GRN}"   ██║   ██╔══╝  ██║╚██╗██║ ██╔██╗     "
+	@echo -e ${CL_GRN}"   ██║   ███████╗██║ ╚████║██╔╝ ██╗    "
+	@echo -e ${CL_GRN}"   ╚═╝   ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝    "
+	@echo -e ""
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
 
 .PHONY: bacon
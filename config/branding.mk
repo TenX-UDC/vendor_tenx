83a9f2e3e0296411d5a694b25e7fb8cc8e93d697
Aston-Martinn <advaithbhat9@gmail.com>
2019-09-15 23:52:51 +0530

    vendor: Init TenX-OS based on Lineage-OS
    Change-Id: I53c407da455885af7029853ff635090f34b01f0c
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

---
+++ config/branding.mk
@@ -0,0 +1,31 @@
+# Branding
+TENX_VERSION := 1.3
+TENX_BASE_VERSION := v1.3
+TENX_FANBASE_NAME := Alpha
+
+TENX_BUILD_TYPE ?= Unofficial
+
+TENX_DATE_YEAR := $(shell date -u +%Y)
+TENX_DATE_MONTH := $(shell date -u +%m)
+TENX_DATE_DAY := $(shell date -u +%d)
+TENX_DATE_HOUR := $(shell date -u +%H)
+TENX_DATE_MINUTE := $(shell date -u +%M)
+TENX_BUILD_DATE_UTC := $(shell date -d '$(TENX_DATE_YEAR)-$(TENX_DATE_MONTH)-$(TENX_DATE_DAY) $(TENX_DATE_HOUR):$(TENX_DATE_MINUTE) UTC' +%s)
+TENX_BUILD_DATE := $(TENX_DATE_YEAR)$(TENX_DATE_MONTH)$(TENX_DATE_DAY)-$(TENX_DATE_HOUR)$(TENX_DATE_MINUTE)
+TENX_BUILD_DATETIME := $(shell date +%s)
+
+TENX_PLATFORM_VERSION := 15.0
+
+TARGET_PRODUCT_SHORT := $(subst lineage_,,$(LINEAGE_BUILD))
+
+TENX_VERSION := TenX-OS-$(TENX_BASE_VERSION)_$(LINEAGE_BUILD)-$(TENX_PLATFORM_VERSION)-$(TENX_BUILD_DATE)-$(TENX_FANBASE_NAME)-$(TENX_BUILD_TYPE)
+TENX_VERSION_PROP := fifteen
+
+# TenX-OS version properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    org.tenx_version=$(TENX_BASE_VERSION) \
+    org.tenx.fanbase_name=$(TENX_FANBASE_NAME) \
+    org.tenx.version.display=$(TENX_VERSION) \
+    org.tenx.build_date=$(TENX_BUILD_DATE) \
+    org.tenx.build_date_utc=$(TENX_BUILD_DATE_UTC) \
+    org.tenx.build_type=$(TENX_BUILD_TYPE)
